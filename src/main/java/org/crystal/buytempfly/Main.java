9e4fb3e3887e9c17c90ffa0548067d202b05afca
Crystal Moling <clovermoling@gmail.com>
2021-09-20 11:59:30 +0800

    Complete basic functions

--- src/main/java/org/crystal/buytempfly/Main.java
+++ src/main/java/org/crystal/buytempfly/Main.java
@@ -10,28 +10,58 @@ import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-    private net.milkbowl.vault.economy.Economy vault;
+    private static net.milkbowl.vault.economy.Economy vault = null;
     private double prize = 100.0;
-
     public void onEnable() {
         if(!initVault()){
             getLogger().info("vault插件挂钩失败，请检查是否安装了vault插件");
+        } else if (!setupEconomy()){
+            getLogger().info("vault插件加载失败，请检查是否安装了vault插件");
         } else {
             getLogger().info("BuyTempFly插件加载成功!");
         }
     }
-
     private boolean initVault(){
         RegisteredServiceProvider<Economy> economyProvider
                 = getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
         if(economyProvider == null) return false;
         else return true;
     }
+    private boolean setupEconomy() {
+        RegisteredServiceProvider<net.milkbowl.vault.economy.Economy> economyProvider;
+        try {
+            economyProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
+        } catch (Exception e) {
+            return false;
+        }
+
+        if (economyProvider != null) {
+            this.vault = economyProvider.getProvider();
+        }
+
+        if (this.vault == null) {
+            return false;
+        }
+
+        if (this.vault.getName() == null || this.vault.getName().isEmpty()) {
+
+                    getLogger()
+                    .warning(
+                            "Current economy plugin not correct process all request, this usually cause by irregular code, you should report this issue to your economy plugin author or use other economy plugin.");
+
+                    getLogger()
+                    .warning(
+                            "This is technical information, please send this to economy plugin author: "
+                                    + "VaultEconomyProvider.getName() return a null or empty.");
+        } else {
+            getLogger().info("Using economy system: " + this.vault.getName());
+        }
+        return true;
+    }
 
     private void command(String Command){
         Bukkit.dispatchCommand(Bukkit.getConsoleSender(), Command);
     }
-
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player player = (Player) sender;
         if (!(sender instanceof Player)) {
