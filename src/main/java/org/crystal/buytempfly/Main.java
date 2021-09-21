ef4950acc3dcfa3e62cef73661a43bc1467bdc53
T4M-Moling <clovermoling@gmail.com>
2021-09-21 23:59:10 +0800

    Update

--- src/main/java/org/crystal/buytempfly/Main.java
+++ src/main/java/org/crystal/buytempfly/Main.java
@@ -1,5 +1,6 @@
 package org.crystal.buytempfly;
 
+import com.moneybags.tempfly.TempFly;
 import net.milkbowl.vault.economy.Economy;
 
 import org.bukkit.Bukkit;
@@ -8,6 +9,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.UUID;
 
 public class Main extends JavaPlugin {
     public boolean def;
@@ -21,9 +24,9 @@ public class Main extends JavaPlugin {
 
     public void onEnable() {
         if(!initVault()){
-            getLogger().info("§b vault插件挂钩失败，请检查是否安装了vault插件");
+            getLogger().info("§b vault插件挂钩失败，请检查是否安装了vault插件及EssentialsX插件");
         } else if (!setupEconomy()){
-            getLogger().info("§b vault插件加载失败，请检查是否安装了vault插件");
+            getLogger().info("§b vault插件加载失败，请检查是否安装了vault插件及EssentialsX插件");
         } else {
             InitConfig();
             getLogger().info("§b BuyTempFly插件加载成功!");
@@ -41,13 +44,17 @@ public class Main extends JavaPlugin {
             this.saveDefaultConfig();
             getLogger().info("§b 初次启动，请修改config.yml!");
         }
-    }
+    }  //初始化配置
     private boolean initVault(){
         RegisteredServiceProvider<Economy> economyProvider
                 = getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
         if(economyProvider == null) return false;
         else return true;
-    }
+    }  //初始化Vault插件
+    private UUID getPlayerUUID(Player player){
+        String fullPlayer = "OfflinePlayer:" + player.getName();
+        return UUID.nameUUIDFromBytes(fullPlayer.getBytes());
+    }  //初始化TempFly插件
     private boolean setupEconomy() {
         RegisteredServiceProvider<net.milkbowl.vault.economy.Economy> economyProvider;
         try {
@@ -80,26 +87,29 @@ public class Main extends JavaPlugin {
         return true;
     }
 
-    private void command(String Command){
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), Command);
+    private boolean command(String Command){
+        return Bukkit.dispatchCommand(Bukkit.getConsoleSender(), Command);
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player player = (Player) sender;
-        if (!(sender instanceof Player)) {
+        if (!(sender instanceof Player)) {  //判断执行者是否为玩家
             sender.sendMessage("只有玩家能够使用该命令!");
         } else {
             if (cmd.getName().equalsIgnoreCase("btf")) {
                 if(initVault()){
-                    double bal = vault.getBalance(player);
-                    if(vault.has(player, prize)){
-                        command("eco take " + player.getName() + " " + prize);
-                        command("tf give " + player.getName() + " -d " + FlyD + " -h " + FlyH + " -m " + FlyM + " -s " + FlyS);
+                    double bal = vault.getBalance(player);  //获取玩家余额
+                    if(vault.has(player, prize)){  //判断玩家是否有足够金钱
+                        if (TempFly.getAPI().addFlightTime(getPlayerUUID(player), FlyS) == true){
+                            vault.withdrawPlayer(player, prize);  //扣钱
+                        } else {
+                            player.sendMessage("§a 余额不足,你现在有$" + bal + ",需要$" + prize);
+                        }
                     }else{
-                        player.sendMessage("§a 余额不足,你现在有$" + bal + ",需要$" + prize);
+                        player.sendMessage("§a 飞行时间添加失败!");
                     }
                 } else {
-                    player.sendMessage("§b vault插件挂钩失败，请联系管理员解决问题");
+                    getLogger().info("§b vault插件挂钩失败，请联系管理员解决问题");
                 }
                 return true;
             }
